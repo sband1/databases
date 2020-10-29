c84c54a19a0309a1878b9568fb48c3de3af8b84f
sband1 <sbandeali1@gmail.com>
2020-10-28 22:50:17 -0700

    Implement schema design and imported client files

--- server/schema.sql
+++ server/schema.sql
@@ -3,10 +3,31 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  message_id INT(30) NOT NULL AUTO_INCREMENT,
+  msg_txt VARCHAR(140) NOT NULL,
+  msg_createdAt DATETIME,
+  user_id INT,
+  room_id INT,
+  PRIMARY KEY (message_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (room_id) REFERENCES rooms(user_id),
 );
 
 /* Create other tables and define schemas for them here! */
+
+CREATE TABLE users (
+  user_id INT(5) NOT NULL AUTO_INCREMENT,
+  username VARCHAR(30) NULL, -- anonymous users?
+  user_createdAt DATETIME, -- (fsp)
+  PRIMARY KEY (user_id), -- CONSTRAINT <alias>? need?
+);
+
+CREATE TABLE rooms (
+  room_id INT(5) NOT NULL AUTO_INCREMENT,
+  room_name VARCHAR(30) NULL, -- anonymous users?
+  room_createdAt DATETIME, -- (fsp)
+  PRIMARY KEY (room_id),
+);
 
 
 
